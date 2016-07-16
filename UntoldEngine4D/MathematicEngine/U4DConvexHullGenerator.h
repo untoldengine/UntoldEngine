83e483bca7e5a318129ce98d00d92f9d549ef88b
Harold Serrano <harold.serrano@gmail.com>
2016-07-16 07:40:04 -0700

    created convex hull class
    Starting the development of the convex hull generator class

---
+++ UntoldEngine4D/MathematicEngine/U4DConvexHullGenerator.h
@@ -0,0 +1,30 @@
+//
+//  U4DConvexHullGenerator.hpp
+//  UntoldEngine
+//
+//  Created by Harold Serrano on 7/16/16.
+//  Copyright © 2016 Untold Game Studio. All rights reserved.
+//
+
+#ifndef U4DConvexHullGenerator_hpp
+#define U4DConvexHullGenerator_hpp
+
+#include <stdio.h>
+
+namespace U4DEngine {
+    
+    class U4DConvexHullGenerator{
+        
+    private:
+        
+        
+    public:
+        
+        U4DConvexHullGenerator();
+        ~U4DConvexHullGenerator();
+        
+    }
+
+}
+
+#endif /* U4DConvexHullGenerator_hpp */
---
+++ UntoldEngine4D/MathematicEngine/U4DConvexHullGenerator.mm
@@ -0,0 +1,9 @@
+//
+//  U4DConvexHullGenerator.cpp
+//  UntoldEngine
+//
+//  Created by Harold Serrano on 7/16/16.
+//  Copyright © 2016 Untold Game Studio. All rights reserved.
+//
+
+#include "U4DConvexHullGenerator.h"
