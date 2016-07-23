e42af817a9abbe48721c4ab885069d3e819db988
Harold Serrano <harold.serrano@gmail.com>
2016-07-23 14:14:12 -0700

    Fixed the viewInDirection method
    The viewInDirection method works well for all objects.

--- UntoldEngine4D/Camera/U4DCamera.mm
+++ UntoldEngine4D/Camera/U4DCamera.mm
@@ -22,7 +22,7 @@ U4DCamera* U4DCamera::instance=0;
 U4DCamera::U4DCamera(){
     
     setEntityType(CAMERA);
-    U4DVector3n viewInDirection(0,0,1);
+    U4DVector3n viewInDirection(0,0,-1);
     setViewDirection(viewInDirection);
 }
     
