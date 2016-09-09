86670a72d8fa71dc61a28960ecbc4906b43ebb7e
Harold Serrano <harold.serrano@gmail.com>
2016-09-09 08:59:31 -0500

    Documented OpenGLSprite

--- UntoldEngine4D/OpenGLManager/U4DOpenGLSprite.h
+++ UntoldEngine4D/OpenGLManager/U4DOpenGLSprite.h
@@ -14,19 +14,41 @@
 #include "U4DOpenGLManager.h"
 
 namespace U4DEngine {
-    
+  
+/**
+ @brief The U4DOpenGLSprite class is in charge of rendering sprite entities
+ */
 class U4DOpenGLSprite:public U4DOpenGLImage{
     
 private:
     
+    /**
+     @brief Pointer representing the sprite entity
+     */
     U4DImage* u4dObject;
     
 public:
     
+    /**
+     @brief Constructor for the class
+     
+     @param uU4DImage It takes as a paramenter the entity representing the sprite entity
+     */
     U4DOpenGLSprite(U4DImage* uU4DImage);
     
+    /**
+     @brief Destructor for the class
+     */
     ~U4DOpenGLSprite();
     
+    /**
+     @brief Method which sets the image(texture) dimension found in an atlas texture
+     
+     @param uWidth       Image width
+     @param uHeight      Image height
+     @param uAtlasWidth  Atlas width
+     @param uAtlasHeight Atlas height
+     */
     void setImageDimension(float uWidth,float uHeight, float uAtlasWidth,float uAtlasHeight);
     
 };
