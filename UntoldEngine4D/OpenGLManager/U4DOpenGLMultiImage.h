23317fdde9bf107fc5f88ad11c30bec44cf3730f
Harold Serrano <harold.serrano@gmail.com>
2016-09-09 08:52:41 -0500

    Documented OpenGLMultiImage

--- UntoldEngine4D/OpenGLManager/U4DOpenGLMultiImage.h
+++ UntoldEngine4D/OpenGLManager/U4DOpenGLMultiImage.h
@@ -13,22 +13,56 @@
 #include "U4DOpenGLImage.h"
 #include "U4DOpenGLManager.h"
 
+
 namespace U4DEngine {
-    
+
+/**
+ @brief The U4DOpenGLMultiImage class is in charge of rendering multi-image entities
+ */
 class U4DOpenGLMultiImage:public U4DOpenGLImage{
     
 private:
-    
+    /**
+     @brief Inform the engine to activate multi-image(texture) support
+     */
     bool activateMultiTextureImage;
     
 public:
+    
+    /**
+     @brief Constructor for the class
+     
+     @param uU4DImage It takes as a paramenter the entity representing the multi-image entity
+     */
     U4DOpenGLMultiImage(U4DImage *uU4DImage);
+    
+    /**
+     @brief Destructor for the class
+     */
     ~U4DOpenGLMultiImage();
     
+    /**
+     @brief Method which activates the Texture Units used in rendering
+     */
     void activateTexturesUniforms();
+    
+    /**
+     @brief Method which loads all Texture Object Buffers used in rendering
+     */
     void loadTextureObjectBuffer();
+    
+    /**
+     @brief Method which sets the ambient-texture used for the entity
+     
+     @param uTexture Name of the ambient-texture
+     */
     virtual void setAmbientTexture(const char* uTexture);
     
+    /**
+     @brief Method which asks the engine to enable multi-image
+     
+     @param value Boolean variable to enable/disable multi-image support
+     */
     void setMultiImageActiveImage(bool value);
 };
     
