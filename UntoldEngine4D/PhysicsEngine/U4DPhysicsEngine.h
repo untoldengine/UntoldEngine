be039ad533775e68e68f3f57dfdaa83ccbc57aad
Harold Serrano <harold.serrano@gmail.com>
2016-09-17 11:06:07 -0700

    documented the physics engine class

--- UntoldEngine4D/PhysicsEngine/U4DPhysicsEngine.h
+++ UntoldEngine4D/PhysicsEngine/U4DPhysicsEngine.h
@@ -19,18 +19,34 @@
 #include "U4DDragForceGenerator.h"
 #include "U4DRestingForcesGenerator.h"
 
+
 namespace U4DEngine {
-    
+
+    /**
+     @brief The U4DPhysicsEngine class is in charge of implemeting the physics engine operations
+     */
     class U4DPhysicsEngine{
       
     private:
-        
+       
+        /**
+         @brief Object in charge of integrating the equation of motion
+         */
         U4DIntegrator *integrator;
         
+        /**
+         @brief Object in charge of simulating the force of gravity
+         */
         U4DGravityForceGenerator gravityForce;
         
+        /**
+         @brief Object in charge of simulating the drag force
+         */
         U4DDragForceGenerator dragForce;
         
+        /**
+         @brief Object in charge of simulating resting forces
+         */
         U4DRestingForcesGenerator restingForces;
         
     protected:
@@ -38,17 +54,44 @@ namespace U4DEngine {
         
     public:
         
+        /**
+         @brief Constructor for the class
+         */
         U4DPhysicsEngine();
         
+        /**
+         @brief Destructor for the class
+         */
         ~U4DPhysicsEngine();
         
-        //run the physics
+        /**
+         @brief Method which updates all physics forces
+         
+         @param uModel 3D model entity
+         @param dt     Time-step value
+         */
         void updatePhysicForces(U4DDynamicModel *uModel,float dt);
         
+        /**
+         @brief Method which sets the integrator
+         
+         @param uIntegrator Pointer to the integrator to use
+         */
         void setIntegrator(U4DIntegrator *uIntegrator);
         
+        /**
+         @brief Method which integrates the equation of motion
+         
+         @param uModel 3D model entity
+         @param dt     Time-step value
+         */
         void integrate(U4DDynamicModel *uModel,float dt);
         
+        /**
+         @brief Method which updates the state of the physics engine
+         
+         @param dt Time-step value
+         */
         void update(float dt);
         
         
