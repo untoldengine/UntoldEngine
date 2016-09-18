2307e8e0d3bcd4b961b9fffd67b7edbda0d57eb4
Harold Serrano <harold.serrano@gmail.com>
2016-09-18 09:29:32 -0700

    Documented the contact manifold class

--- UntoldEngine4D/PhysicsEngine/CollisionEngine/ManifoldGeneration/U4DManifoldGeneration.h
+++ UntoldEngine4D/PhysicsEngine/CollisionEngine/ManifoldGeneration/U4DManifoldGeneration.h
@@ -13,17 +13,46 @@
 #include "U4DCollisionDetection.h"
 
 namespace U4DEngine {
-    
+
+    /**
+     @brief The U4DManifoldGeneration is in charge of computing collision maniforld information
+     */
     class U4DManifoldGeneration:public U4DCollisionDetection{
+    
     private:
         
     public:
+    
+        /**
+         @brief Constructor for the class
+         */
         U4DManifoldGeneration(){};
         
+        /**
+         @brief Destructor for the class
+         */
         ~U4DManifoldGeneration(){};
         
+        /**
+         @brief Method which determines the collision manifold. It computes the collision planes
+         
+         @param uModel1                3D model entity
+         @param uModel2                3D model entity
+         @param uQ                     Simplex Data set
+         @param uCollisionManifoldNode Collision Manifold node
+         */
         virtual void determineCollisionManifold(U4DDynamicModel* uModel1, U4DDynamicModel* uModel2,std::vector<SIMPLEXDATA> uQ, COLLISIONMANIFOLDONODE &uCollisionManifoldNode){};
         
+        /**
+         @brief Method which determines the collision contact manifold. It retrieves the collision contact points of the collision.
+         
+         @param uModel1                3D model entity
+         @param uModel2                3D model entity
+         @param uQ                     Simplex Data set
+         @param uCollisionManifoldNode Collision Manifold node
+         
+         @return Returns true if the collision contact points were successfully computed
+         */
         virtual bool determineContactManifold(U4DDynamicModel* uModel1, U4DDynamicModel* uModel2,std::vector<SIMPLEXDATA> uQ,COLLISIONMANIFOLDONODE &uCollisionManifoldNode){};
         
     };
