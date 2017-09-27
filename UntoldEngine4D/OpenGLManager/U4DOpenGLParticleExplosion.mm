5047b6aa56e86c5f9a14d142590ec4e4221a4d56
Harold Serrano <harold.serrano@gmail.com>
2017-09-26 22:39:37 -0700

    Removed all instances to opengl

--- UntoldEngine4D/OpenGLManager/U4DOpenGLParticleExplosion.mm
+++
@@ -1,9 +0,0 @@
-//
-//  U4DOpenGLParticleExplosion.cpp
-//  UntoldEngine
-//
-//  Created by Harold Serrano on 12/19/16.
-//  Copyright © 2016 Untold Game Studio. All rights reserved.
-//
-
-#include "U4DOpenGLParticleExplosion.h"
