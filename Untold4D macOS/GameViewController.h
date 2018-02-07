43eb63d14089e507a3bd2634554feeeded972b0a
Harold Serrano <harold.serrano@gmail.com>
2018-02-07 06:31:33 -0700

    Fixed the bug related to the missing files.

--- Untold4D macOS/GameViewController.h
+++ Untold4D macOS/GameViewController.h
@@ -9,7 +9,7 @@
 #import <Cocoa/Cocoa.h>
 #import <Metal/Metal.h>
 #import <MetalKit/MetalKit.h>
-#import "Renderer.h"
+
 
 // Our macOS view controller.
 @interface GameViewController : NSViewController
