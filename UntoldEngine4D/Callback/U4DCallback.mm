e39a86f111e2cd7870d465b116bc55cb45dd7b35
Harold Serrano <harold.serrano@gmail.com>
2018-03-27 06:43:08 -0700

    Moved the callback implementation to the header

--- UntoldEngine4D/Callback/U4DCallback.mm
+++ UntoldEngine4D/Callback/U4DCallback.mm
@@ -13,75 +13,7 @@
 
 namespace U4DEngine {
     
-    template <class T>
-    U4DCallback<T>::U4DCallback(){
-        pClass=0;
-        pMethod=0;
-        
-    }
     
-    template <class T>
-    void U4DCallback<T>::setTimer(U4DTimer *uTimer){
-        
-        timer=uTimer;
-    }
-    
-    template <class T>
-    void U4DCallback<T>::setClass(T* uClass){
-        
-        pClass=uClass;
-        
-    }
-    
-    template <class T>
-    void U4DCallback<T>::setMethod(pAction uMethod){
-        
-        pMethod=uMethod;
-        
-    }
-    
-    template <class T>
-    void U4DCallback<T>::scheduleClassWithMethodAndDelay(T* uClass, pAction uMethod,U4DTimer* uTimer,bool repeat){
-        
-    }
-    
-    template <class T>
-    void U4DCallback<T>::scheduleClassWithMethodAndDelay(T* uClass, pAction uMethod,U4DTimer* uTimer, double delay,bool repeat)
-    {
-        pClass=uClass;
-        pMethod=uMethod;
-        timer=uTimer;
-        
-        timer->setDelay(delay);
-        timer->setRepeat(repeat);
-        timer->setCurrentTime(0.0);
-        timer->setPause(false);
-        
-        //register U4DTimer with U4DScheduler
-        U4DScheduler *scheduler=U4DScheduler::sharedInstance();
-        scheduler->scheduleTimer(timer);
-    }
-    
-    template <class T>
-    void U4DCallback<T>::scheduleClassWithMethod(T* uClass, pAction uMethod)
-    {
-        pClass=uClass;
-        pMethod=uMethod;
-    }
-    
-    template <class T>
-    void U4DCallback<T>::unScheduleTimer(U4DTimer* uTimer){
-        
-        //register U4DTimer with U4DScheduler
-        U4DScheduler *scheduler=U4DScheduler::sharedInstance();
-        scheduler->unscheduleTimer(uTimer);
-    }
-    
-    template <class T>
-    void U4DCallback<T>::action(){
-        
-        (pClass->*pMethod)();
-    }
     
 }
 
