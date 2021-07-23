6e3f9dcebaa85988c573eb4c20894e17da440ef2
Harold Serrano <harold.serrano@untoldengine.com>
2021-07-22 22:56:51 -0700

    changed the imgui style

--- UntoldEngine4D/Imgui/imgui.h
+++ UntoldEngine4D/Imgui/imgui.h
@@ -301,7 +301,7 @@ namespace ImGui
 
     // Styles
     IMGUI_API void          StyleColorsDark(ImGuiStyle* dst = NULL);    // new, recommended style (default)
-    IMGUI_API void          StyleColorsDark2(ImGuiStyle* dst = NULL);    // new, recommended style (default)
+    IMGUI_API void          StyleColorYellow(ImGuiStyle* dst = NULL);    // new, recommended style (default)
     IMGUI_API void          StyleColorsLight(ImGuiStyle* dst = NULL);   // best used with borders and a custom, thicker font
     IMGUI_API void          StyleColorsClassic(ImGuiStyle* dst = NULL); // classic imgui style
 
