639d868cde701b5b73eda29afa2f32d504e49ccd
Harold Serrano <harold.serrano@untoldengine.com>
2025-01-02 14:21:56 -0700

    set default ibl

--- Sources/UntoldEngine/Utils/Globals.swift
+++ Sources/UntoldEngine/Utils/Globals.swift
@@ -140,7 +140,7 @@ let boundingBoxVertexCount = 24
 var selectedModel: Bool = false
 
 var envRotationAngle: Float = 0
-public var hdrURL: String = "potsdamer_platz_2k.hdr"
+public var hdrURL: String = "photostudio.hdr"
 public var resourceURL: URL?
 
 var currentGlobalTime: Float = 0.0
