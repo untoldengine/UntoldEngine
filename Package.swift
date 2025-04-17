fd81d071fbd398a9ef01bc90ca4b157f8df2aac3
Harold Serrano <harold.serrano@untoldengine.com>
2025-04-17 06:48:53 -0700

    [Patch] updated version number for platform

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "UntoldEngine",
     platforms: [
-        .macOS(.v10_15),
+        .macOS(.v13),
     ],
     products: [
         // Library product for the engine
