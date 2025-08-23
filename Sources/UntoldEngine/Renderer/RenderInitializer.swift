1a3d353d79faa43daab596856cf1b498423fdc96
Harold Serrano <harold.serrano@untoldengine.com>
2025-08-23 09:25:32 -0700

    [Patch] added create compute pipeline

--- Sources/UntoldEngine/Renderer/RenderInitializer.swift
+++ Sources/UntoldEngine/Renderer/RenderInitializer.swift
@@ -73,6 +73,35 @@ func createPipeline(
     }
 }
 
+func createComputePipeline(
+    into pipeline: inout ComputePipeline,
+    device: MTLDevice,
+    library: MTLLibrary,
+    functionName: String,
+    pipelineName: String
+) {
+    // Create kernel
+    guard let function = library.makeFunction(name: functionName) else {
+        pipeline.name = pipelineName
+        handleError(.kernelCreationFailed, pipelineName)
+        return
+    }
+
+    // Create pipeline
+    do {
+        let state = try device.makeComputePipelineState(function: function)
+
+        pipeline.pipelineState = state
+        pipeline.name = pipelineName
+        pipeline.success = true
+    } catch {
+        pipeline.name = pipelineName
+        pipeline.success = false
+        handleError(.pipelineStateCreationFailed, pipelineName)
+        return
+    }
+}
+
 func createBuffer<T>(
     device: MTLDevice,
     data: [T],
