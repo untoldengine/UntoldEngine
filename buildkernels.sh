59cf6a4ccaa97745e72aef938957d2d0c25aaa42
Harold Serrano <harold.serrano@untoldengine.com>
2025-09-19 23:46:49 -0700

    [Patch] Separated kernels according to target

--- buildkernels.sh
+++ buildkernels.sh
@@ -2,6 +2,12 @@
 
 cd Sources/UntoldEngine/UntoldEngineKernels
 
+# mac (device)
 xcrun -sdk macosx metal UntoldEngineKernels.metal -c -o UntoldEngineKernels.air
 
 xcrun -sdk macosx metallib UntoldEngineKernels.air -o UntoldEngineKernels.metallib
+
+# iOS (device)
+xcrun -sdk iphoneos metal UntoldEngineKernels.metal -c -o UntoldEngineKernels-ios.air
+
+xcrun -sdk iphoneos metallib UntoldEngineKernels-ios.air -o UntoldEngineKernels-ios.metallib
