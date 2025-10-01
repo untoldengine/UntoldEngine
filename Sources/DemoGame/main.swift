421b2304c382cddcab756dddc9e815fb8a8d2821
Javier Segura <javier@miolabs.com>
2025-10-01 17:40:51 +0200

    Adding back creation of defatult entities like game camera or direct light

--- Sources/DemoGame/main.swift
+++ Sources/DemoGame/main.swift
@@ -121,8 +121,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
         self.renderer = renderer
 
-        renderer.initResources()
-
         // Step 3. Create the game scene and connect callbacks
         gameScene = GameScene()
         renderer.setupCallbacks(
