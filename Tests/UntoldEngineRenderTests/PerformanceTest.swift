269ea511f151d59bfc39bebc04cd89c73a1617f2
Harold Serrano <harold.serrano@untoldengine.com>
2025-10-21 22:15:28 -0700

    [Bugfix] Updated test to use usdz files

--- Tests/UntoldEngineRenderTests/PerformanceTest.swift
+++ Tests/UntoldEngineRenderTests/PerformanceTest.swift
@@ -77,12 +77,12 @@ final class PerformanceTests: XCTestCase {
 
         // Stadium (static mesh)
         let stadium = createEntity()
-        setEntityMesh(entityId: stadium, filename: "stadium", withExtension: "usdc")
+        setEntityMesh(entityId: stadium, filename: "stadium", withExtension: "usdz")
         translateBy(entityId: stadium, position: simd_float3(0.0, 0.0, 0.0))
 
         // Player (animated, named for lookup)
         let player = createEntity()
-        setEntityMesh(entityId: player, filename: "redplayer", withExtension: "usdc", flip: false)
+        setEntityMesh(entityId: player, filename: "redplayer", withExtension: "usdz", flip: false)
         setEntityAnimations(entityId: player, filename: "running", withExtension: "usdc", name: "running")
         setEntityName(entityId: player, name: "player")
         rotateTo(entityId: player, angle: 0, axis: simd_float3(0.0, 1.0, 0.0))
@@ -91,7 +91,7 @@ final class PerformanceTests: XCTestCase {
 
         // Ball (named for lookup)
         let ball = createEntity()
-        setEntityMesh(entityId: ball, filename: "ball", withExtension: "usdc")
+        setEntityMesh(entityId: ball, filename: "ball", withExtension: "usdz")
         setEntityName(entityId: ball, name: "ball")
         translateBy(entityId: ball, position: simd_float3(0.0, 0.6, 3.0))
 
