128d97eab059573a5eb09a17f9d7984eb101e8f8
Harold Serrano <harold.serrano@untoldengine.com>
2025-10-22 19:01:13 -0700

    [Test] Improved testing

---
+++ Tests/UntoldEngineRenderTests/AnimationTest.swift
@@ -0,0 +1,15 @@
+//
+//  AnimationTest.swift
+//  UntoldEngine
+//
+//  Copyright (C) Untold Engine Studios
+//  Licensed under the GNU LGPL v3.0 or later.
+//  See the LICENSE file or <https://www.gnu.org/licenses/> for details.
+
+import CShaderTypes
+import simd
+import UniformTypeIdentifiers
+@testable import UntoldEngine
+import XCTest
+
+final class AnimationTests: BaseRenderSetup {}
