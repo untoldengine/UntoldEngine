bc0335b075e4e661c0d38dba050b81088aee6965
Harold Serrano <harold.serrano@untoldengine.com>
2025-11-25 07:27:06 -0700

    [Feature] Initial implementation of building system

---
+++ Sources/UntoldEngine/XcodeGenProjectSpec.swift
@@ -0,0 +1,96 @@
+//
+//  XcodeGenProjectSpec.swift
+//  UntoldEngine
+//
+//  Generates XcodeGen project.yml specification as YAML string
+//
+//  Copyright (C) Untold Engine Studios
+//  Licensed under the GNU LGPL v3.0 or later.
+//
+
+import Foundation
+
+/// Generates XcodeGen project specification for game builds
+struct XcodeGenProjectSpec {
+    
+    /// Generate project.yml YAML content from build settings
+    static func generateYAML(settings: BuildSettings) throws -> String {
+        let platformName = settings.target.platformName
+        let deploymentTarget = settings.target.deploymentTarget
+        
+        var yaml = """
+        name: \(settings.projectName)
+        
+        packages:
+          UntoldEngine:
+            url: https://github.com/untoldengine/UntoldEngine.git
+            branch: develop
+        
+        targets:
+          \(settings.projectName):
+            type: application
+            platform: \(platformName)
+            deploymentTarget: \(deploymentTarget)
+            sources:
+              - path: Sources
+                excludes:
+                  - "\(settings.projectName)/GameData"
+              - path: Sources/\(settings.projectName)/GameData
+                type: folder
+                buildPhase: resources
+            dependencies:
+              - package: UntoldEngine
+            settings:
+              base:
+                PRODUCT_BUNDLE_IDENTIFIER: \(settings.bundleIdentifier)
+                SWIFT_VERSION: 5.0
+                MARKETING_VERSION: "1.0"
+                CURRENT_PROJECT_VERSION: "1"
+                GENERATE_INFOPLIST_FILE: YES
+                INFOPLIST_KEY_LSApplicationCategoryType: public.app-category.games
+        """
+        
+        // Add team ID if provided
+        if let teamID = settings.teamID, !teamID.isEmpty {
+            yaml += """
+            
+                DEVELOPMENT_TEAM: \(teamID)
+            """
+        }
+        
+        // Add configurations
+        yaml += """
+        
+              configs:
+                Debug:
+                  SWIFT_OPTIMIZATION_LEVEL: -Onone
+                  DEBUG_INFORMATION_FORMAT: dwarf-with-dsym
+                Release:
+                  SWIFT_COMPILATION_MODE: wholemodule
+        """
+        
+        // Add optimization level for release
+        let optLevel: String
+        switch settings.optimizationLevel {
+        case .none: optLevel = "-Onone"
+        case .speed: optLevel = "-O"
+        case .size: optLevel = "-Osize"
+        }
+        
+        yaml += """
+        
+                  SWIFT_OPTIMIZATION_LEVEL: \(optLevel)
+        """
+        
+        // Add debug info for release if requested
+        if settings.includeDebugInfo {
+            yaml += """
+            
+                  DEBUG_INFORMATION_FORMAT: dwarf-with-dsym
+            """
+        }
+        
+        return yaml
+    }
+}
+
