efcf3ebe8de9792c4f46584d772699f63ea0d3eb
Untold Engine <harold.serrano@untoldengine.com>
2026-05-21 10:10:16 -0700

    [Release] Preparing release 0.12.13

--- Sources/Sandbox/AppDelegate.swift
+++ Sources/Sandbox/AppDelegate.swift
@@ -10,7 +10,7 @@
     @MainActor
     final class AppDelegate: NSObject, NSApplicationDelegate {
         private enum Constants {
-            static let appVersion = "0.12.12"
+            static let appVersion = "0.12.13"
             static let windowSize = NSSize(width: 1600, height: 900)
         }
 
