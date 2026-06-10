c9bf28b2bdbf6e976b063adcb66183d41378d6fc
Untold Engine <harold.serrano@untoldengine.com>
2026-06-10 06:27:19 -0700

    [Release] Preparing release 0.13.1

--- README.md
+++ README.md
@@ -76,7 +76,7 @@ Clone the repository and launch the demo:
 ```bash
 git clone https://github.com/untoldengine/UntoldEngine.git
 cd UntoldEngine
-git checkout v0.13.0
+git checkout v0.13.1
 swift run untolddemo
 ```
 
--- Sources/UntoldEngine/Renderer/UntoldEngine.swift
+++ Sources/UntoldEngine/Renderer/UntoldEngine.swift
@@ -181,7 +181,7 @@ public class UntoldRenderer: NSObject, MTKViewDelegate {
             BatchingSystem.shared.applyRuntimeBatchingTuning(.macOSBalanced)
         #endif
 
-        Logger.log(message: "Untold Engine Starting. Version 0.13.0")
+        Logger.log(message: "Untold Engine Starting. Version 0.13.1")
     }
 
     public func initSizeableResources() {
